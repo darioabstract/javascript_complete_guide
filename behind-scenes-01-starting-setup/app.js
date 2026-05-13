2cca1d9b66b7558fb25b693c7e3258c857fc1b0b
Dario Milani <dario.milani@abstract.it>
2026-05-13 15:14:32 +0200

    completed for loops module

---
+++ behind-scenes-01-starting-setup/app.js
@@ -0,0 +1,28 @@
+
+// var name = 'Dario';
+
+// {
+//     let test = 5;
+//     console.log(test)
+// }
+
+// if (name === 'Dario') {
+//     let hobbies = ['sports', 'cooking'];
+//     console.log(hobbies)
+// }
+
+
+// const greet = () => {
+//     var age = 41;
+//     var name = 'Max';
+//     console.log(name, age, hobbies);
+// }
+
+// console.log(name, hobbies);
+
+// greet()
+'use strict'
+var userName = 'Dario';
+
+
+console.log(userName);

12bcc7ad56434c70f49f6e7073af95d9c80daa06
Dario Milani <dario.milani@abstract.it>
2026-06-04 16:27:05 +0200

    working with classes and constructor in oop

---
+++ oop-01-starting-setup/assets/scripts/app.js
@@ -0,0 +1,57 @@
+class Product {
+    // title = 'DEFAULT';
+    // imageUrl;
+    // description;
+    // price;
+
+    constructor(title, image, desc, price) {
+        this.title = title;
+        this.imageUrl = image;
+        this.description = desc;
+        this.price = price;
+    }
+}
+
+
+const productList = {
+    products: [
+        new Product(
+            'A Pillow',
+            'https://media.tempur.com/i/tempursealy/ITARCHSCM/Cuscino-TEMPUR-Arch-SmartCool?$pdp-poi$',
+            'A soft pillow!',
+            19.99,
+        ),
+        new Product(
+            'A Carpet',
+            'https://rugeditorial.com/cdn/shop/articles/Screenshot_2026-02-18_at_12.20.53_PM.png?v=1771397482&width=1000',
+            'A carpet which you might like - or not',
+            89.99,
+        )
+
+    ],
+    render() {
+        const renderHook = document.getElementById('app');
+        const prodList = document.createElement('ul');
+        prodList.className = 'product-list';
+        for (const prod of this.products) {
+            const prodEl = document.createElement('li');
+            prodEl.className = 'product-item';
+            prodEl.innerHTML = `
+            <div>
+            <img src="${prod.imageUrl}" alt="${prod.title}">
+            <div class="product-item__content">
+            <h2>${prod.title}</h2>
+            <h3>\$${prod.price}</h3>
+            <p>${prod.description}</p>
+            <button>Add to Cart</button>
+            </div>
+            `
+            prodList.append(prodEl);
+        }
+        renderHook.append(prodList);
+    }
+
+
+}
+
+productList.render();

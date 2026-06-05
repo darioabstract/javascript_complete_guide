e436a2981da60e0c4832f69edf5b641d4a0e90ca
Dario Milani <dario.milani@abstract.it>
2026-06-05 10:18:20 +0200

    dom assignment

---
+++ dom-assignment-start/assignment.js
@@ -0,0 +1,19 @@
+// Task One
+const task1El1 = document.getElementById('task-1');
+const task1el2 = document.querySelector('li');
+
+
+task1El1.style.backgroundColor = 'black';
+task1el2.style.color = 'white';
+
+//Task Two
+const task2 = document.querySelector('head title').textContent = 'Assignment - Solved';
+
+const task2_1 = document.querySelector('title').textContent = 'Assignment - Solved';
+
+const task2_2 = document.querySelector('head').querySelector('title');
+
+
+//task Three
+
+const h1 = document.querySelector('h1').textContent = 'Assignment - Solved'
